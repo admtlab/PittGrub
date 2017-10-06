24e4ecc6ff0144a36ac4a9df30a3c1fd82ee949d
Mark Silvis <marksilvis@pitt.edu>
2017-10-06 06:37:16 -0400

    updates login screen with information from index

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,120 +1,184 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, StatusBar, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
-import { FormLabel, FormInput, CheckBox, Button, Grid, Col, Slider } from 'react-native-elements';
-import { NavigationActions } from 'react-navigation'
-import { Tabs } from '../containers/Route'
+import { ActivityIndicator, KeyboardAvoidingView, StatusBar, StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { Button, Col, Grid } from 'react-native-elements';
+import { NavigationActions } from 'react-navigation';
 import settings from '../config/settings';
+import { postLogin } from '../lib/api';
+import { storeToken, storeUser } from '../lib/auth';
+import { registerForPushNotifications } from '../lib/notifications';
 
-const LOGIN_ENDPOINT = settings.server.url + '/login';
 
-export default class Login extends React.Component {
+export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      loading: false,
+      correctCredentials: false,
     }
-    this.login = this.login.bind(this);
+
+    this._login = this._login.bind(this);
   }
 
-  login() {
+  _login = async () => {
     if (this.state.email !== '' && this.state.password !== '') {
-      fetch(LOGIN_ENDPOINT, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: this.state.email,
-          password: this.state.password,
-        }),
-      })
-      .then((response) => response.json())
-      .then((responseData) => {
-        global.user_id = responseData['user'];
-      })
-      .catch((error) => {
-        console.log('failed login');
-      }).done(() => {
-        console.log('global user id is: ' + global.user_id);
-        console.log('navigating to tabs');
-        this.props.navigation.navigate('Tab');
-      });
+      let activated = false;
+      let status = '';
+      postLogin(this.state.email, this.state.password)
+        .then((response) => response.json())
+        .then((responseData) => {
+          if (responseData.status !== undefined && responseData.status >= 400) {
+            console.log('printing message')
+            const message = responseData['message'];
+            console.log(message);
+          } else {
+            console.log(responseData);
+            this.setState({ loading: false });
+            activated = responseData['user']['active'];
+            status = responseData['user']['status'];
+            console.log('storing token');
+            storeToken({ token: responseData['token'], expires: responseData['expires'] });
+            console.log('storing user');
+            storeUser(responseData['user']);
+            console.log('setting global');
+            global.admin = responseData['user']['admin'];
+            if (!activated) {
+              console.log('not activated');
+              registerForPushNotifications();
+              this.props.navigation.navigate('Verification');
+            } else if (status !== "ACCEPTED") {
+              console.log('near waiting');
+              this.props.navigation.navigate('Waiting');
+            } else {
+              console.log('near home');
+              this.props.navigation.navigate('Home');
+            }
+          }
+        })
+        .catch((error) => {
+          console.log('error loggin ing');
+          console.log(error);
+        })
+        .done(() => {
+          this.setState({ loading: false });
+        });
     }
   }
 
   render() {
-    return(
-      <KeyboardAvoidingView behavior = "padding" style={styles.container}>
-      <StatusBar barStyle = 'dark-content' />
-      <Grid style={{
-        flex: 0,
-        paddingTop: 200}}>
+    return (
+      <View
+        style={{ flex: 1, backgroundColor: '#333333' }}>
+        {this.props.children}
         <TextInput
           style={styles.input}
-          placeholder='Email'            
-          inputStyle={{color: '#607D8B', fontSize: 15}}
+          marginTop={300}
+          marginLeft={60}
+          placeholder="Email"
+          placeholderTextColor='#444'
+          inputStyle={{ fontSize: 20 }}
           returnKeyType="next"
-          keyboardType='email-address'
+          autoCapitalize='none'
           autoCorrect={false}
-          autoCapitalize="none"
-          onChangeText={(text) => this.setState({ email: text })}
-        />
+          onChangeText={(text) => this.setState({ 'email': text })}
+          value={this.state.email} />
         <TextInput
           style={styles.input}
-          placeholder = "Password"
+          marginLeft={60}
+          placeholder="Password"
           secureTextEntry
-          inputStyle={{color: '#607D8B', fontSize: 15}}
-          returnKeyType = "go"
-          onChangeText={(text) => this.setState({ password: text })}>
-        </TextInput>
-      </Grid>
-      <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={() => {
-          this.login();
-          const nav = NavigationActions.navigate({
-            routeName: 'Login',
-            params: {},
-            action: NavigationActions.navigate({routeName: 'Tabs'})
-          })
-          /* this.props.navigation.navigate('Home', {
-            onNavigationStateChange: (prevState, newState) => {
-              this.setState({...this.state, route_index: newState.index})},
-            screenProps: this.state,
-          }); */
-          this.props.navigation.dispatch(nav);
-        }}>
-        <Text style = {styles.loginbutton}>Login</Text>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
+          placeholderTextColor='#444'
+          inputStyle={{ fontSize: 20 }}
+          returnKeyType="go"
+          autoCapitalize='none'
+          autoCorrect={false}
+          onChangeText={(text) => this.setState({ 'password': text })}
+          value={this.state.password} />
+        {!this.state.loading &&
+          <Grid>
+            <Col style={{ height: 0 }}>
+              <Button
+                title="BACK"
+                large
+                raised
+                fontSize={20}
+                color='#333333'
+                height={80}
+                backgroundColor='rgb(247, 229, 59)'
+                onPress={() => this.props.navigation.goBack(null)}
+                style={{ width: 150, height: 80, alignItems: 'center' }} />
+            </Col>
+            <Col style={{ height: 0 }}>
+              <Button
+                title="ENTER"
+                large
+                raised
+                fontSize={20}
+                color='#333333'
+                backgroundColor='rgb(247, 229, 59)'
+                onPress={() => {
+                  this.setState({ loading: true });
+                  this._login();
+                }}
+                style={{ width: 150, height: 80, alignItems: 'center' }} />
+            </Col>
+          </Grid>}
+        {this.state.loading &&
+          <ActivityIndicator
+            color='#fff' />
+        }
+
+        {/*!this.state.loading &&
+                <Button
+                  title="ENTER"
+                  raised
+                  fontSize={18}
+                  color='#333333'
+                  backgroundColor='rgb(247, 229, 59)'
+                  containerStyle={{backgroundColor: 'rgb(247, 229, 59)'}}
+                  onPress={() => {
+                    if (this.state.email !== '' && this.state.password !== '') {
+                      this.setState({ loading: true });
+                    }
+                  }}
+                  style={{height: null, width: 10, alignItems: 'center'}} />
+              }
+              {this.state.loading &&
+                <ActivityIndicator 
+                  color='#fff'
+                />
+              */}
+      </View>
     );
-  }
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: 20,
   },
-  input:{
-    minWidth:300,
-    flexWrap:'wrap',
-    height : 40,
+  input: {
+    borderRadius: 10,
+    minWidth: 80,
+    width: 300,
+    flexWrap: 'wrap',
+    height: 40,
     backgroundColor: 'rgba(204,204,204,0.2)',
-    paddingHorizontal : 10,
-    color:'#333333',
-    marginBottom : 10,
+    paddingHorizontal: 10,
+    color: '#333333',
+    marginBottom: 10,
   },
-  buttonContainer:{
+  buttonContainer: {
     backgroundColor: "#1980b9",
-    paddingVertical:10,
-    marginTop:15,
-    marginBottom:20
+    paddingVertical: 10,
+    marginTop: 15,
+    marginBottom: 20
   },
-  loginbutton:{
+  loginbutton: {
     color: '#ffffff',
-    textAlign:'center',
-    fontWeight:'700'
+    textAlign: 'center',
+    fontWeight: '700'
   }
 });
