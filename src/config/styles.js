532a987f74140a867b3a27ae5a1b33f6e69785f6
Mark Silvis <marksilvis@pitt.edu>
2017-10-12 17:39:53 -0400

    standardize colors

--- src/config/styles.js
+++ src/config/styles.js
@@ -1,7 +1,14 @@
 export const colors = {
-  facebook: '#3b5998',
-  facebook2: '#00838F',
+  // main colors
+  theme: '#F7E53B',
+  text: '#333',
+  softWhite: '#F5F5F5',
+  softBlue: '#4C6AA4',
+  softGrey: '#EEE',
   background: '#F2F2F2',
   lightBackground: '#F8F7F4',
+  // supporting colors
+  facebook: '#3b5998',
+  facebook2: '#00838F',
   transparent: 'rgba(0,0,0,0)',
 };
