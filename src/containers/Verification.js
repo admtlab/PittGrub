5b4220a6130c264f4c37f58315635a070a81ca73
Mark Silvis <masilvis22@gmail.com>
2017-10-13 18:08:05 -0400

    verification UI overhaul

--- src/containers/Verification.js
+++ src/containers/Verification.js
@@ -1,149 +1,253 @@
+/* @flow */
+
 import React from 'react';
-import { ActivityIndicator, Dimensions, Image, StyleSheet, Text, TextInput, View } from 'react-native';
-import { Button, Col, Grid } from 'react-native-elements';
+import { ActivityIndicator, Dimensions, Keyboard, KeyboardAvoidingView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, ButtonIconRight } from '../components/Button';
+import Logo from '../components/Logo';
+import { colors } from '../config/styles';
 import { postVerification } from '../lib/api';
 import { getUser, activateUser } from '../lib/auth';
-import registerForPushNotifications from '../lib/notifications';
-import images from '../config/images';
+import { registerForPushNotifications } from '../lib/notifications';
 
 
+// screen dimensions
 var { width, height } = Dimensions.get('window');
 
 
 export default class VerificationScreen extends React.Component {
   constructor() {
     super();
+
     this.state = {
       loading: false,
       code: ''
     };
 
+    this._keyboardWillShow = this._keyboardWillShow.bind(this);
+    this._keyboardWillHide = this._keyboardWillHide.bind(this);
+    this._clearState = this._clearState.bind(this);
     this._verification = this._verification.bind(this);
+  }
+
+  componentWillMount() {
+    // move verification form out of the way when
+    // keyboard is coming into view
+    this.keyboardWillShowListener = Keyboard.addListener('keyboardWillShow', this._keyboardWillShow);
+
+    // put it back
+    this.keyboardWillHideListener = Keyboard.addListener('keyboardWillHide', this._keyboardWillHide);
+  }
+
+  componentWillUnmount() {
+    this.keyboardWillShowListener.remove();
+    this.keyboardWillHideListener.remove();
+  }
+
+  _keyboardWillShow = () => {
+    if (!this.state.loading) {
+      this.refs.scrollView.scrollTo({ y: 80, animated: true });
+    }
+  }
+
+  _keyboardWillHide = () => {
+    this.refs.scrollView.scrollTo({ y: 0, animated: true });
+  }
+
+  _clearState = async () => {
+    this.setState({
+      code: '',
+      loading: false,
+    });
   }
 
   _verification = () => {
     postVerification(this.state.code)
-    .then((response) => {
-      if (response.ok) {
-        console.log('response is ok');
-        let status = '';
-        activateUser();
-        console.log('user activated');
-        registerForPushNotifications();
-        console.log('did push');
-        getUser()
-        .then((user) => {
-          console.log('got user');
-          console.log(user);
-          status = user.status;
-          if (status !== 'ACCEPTED') {
-            console.log('not accepted');
-            this.props.navigation.navigate('Waiting');
-          } else {
-            console.log('accpted');
-            this.props.navigation.navigate('Home');
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      }
-    })
-    .catch((error) => {
-      console.log('failed activation');
-    })
-    .done(() => {
-      this.setState({ loading: false });
-    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('response is ok');
+          let status = '';
+          activateUser();
+          console.log('user activated');
+          registerForPushNotifications();
+          console.log('did push');
+          getUser()
+            .then((user) => {
+              console.log('got user');
+              console.log(user);
+              status = user.status;
+              if (status !== 'ACCEPTED') {
+                console.log('not accepted');
+                this.props.navigation.navigate('Waiting');
+              } else {
+                console.log('accpted');
+                this.props.navigation.navigate('Home');
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log('failed activation');
+      })
+      .done(() => {
+        this.setState({ loading: false });
+      })
   }
 
   render() {
-    return(
-      <View
-        style={{flex: 1}}>
-          <Image
-            source={images.enter}
-            style={{flexGrow: 1, width: width, height: height, resizeMode: 'cover', alignContent: 'center', alignItems: 'center'}}>
-            {this.props.children}
+    const isEnabled = (this.state.code.length == 6);
+    return (
+      <ScrollView
+        ref='scrollView'
+        scrollEnabled={false}
+        keyboardShouldPersistTaps={'never'}
+        paddingTop={height - 550}
+        paddingBottom={-height + 550}
+        style={styles.container}>
+        <KeyboardAvoidingView
+          behavior='padding'
+          style={styles.view}>
+          <Logo size={width / 4} />
           <TextInput
+            ref="VerificationCode"
             style={styles.input}
-            marginTop={300}
-            marginLeft={60}
-            placeholder="Enter emailed verification"
+            marginTop={20}
+            placeholder="Verification Code"
             maxLength={6}
-            placeholderTextColor = '#333'
-            inputStyle={{fontSize: 20}}
-            returnKeyType = "go"
-            autoCapitalize='characters'
-            onChangeText={(text) => {
-              this.setState({ activationCode: text })
+            placeholderTextColor='#444'
+            inputStyle={{ fontSize: 20 }}
+            returnKeyType="send"
+            autoCapitalize="characters"
+            onChangeText={(text) => this.setState({ code: text })}
+            onSubmitEditing={() => {
+              Keyboard.dismiss();
+              this.setState({ loading: true });
+              this._verification();
+              this.setState({ loading: false });
             }}
-            value={this.state.activationCode}
-          />
-          <Text height={0}>{'\n'}</Text>
-          {!this.state.loading &&          
-            <Grid>
-              <Col style={{height: 0}}>
-                <Button
-                  title="RESEND"
-                  large
-                  raised
-                  fontSize={20}
-                  color='#333333'
-                  backgroundColor='rgb(247, 229, 59)'
-                  onPress={() => this.props.navigation.goBack(null)}
-                  style={{width: 150, height: 80, alignItems: 'center'}} />
-              </Col>
-              <Col style={{height: 0}}>
-                <Button
-                  title="ENTER"
-                  large
-                  raised
-                  fontSize={20}
-                  color='#333333'
-                  backgroundColor='rgb(247, 229, 59)'
-                  onPress={() => {
-                    if (this.state.activationCode !== '' && this.state.activationCode.length == 6) {
-                      this.setState({ loading: true });
-                      this._verification();
-                    }
-                  }}
-                  style={{width: 150, height: 80, alignItems: 'center'}} />
-              </Col>
-            </Grid>}
-          {this.state.loading &&
-            <ActivityIndicator color='#fff' />
-          }
-        </Image>
-      </View>
+            value={this.state.code} />
+          {!this.state.loading &&
+            <View>
+              <Button text="ENTER"
+                disabled={!isEnabled}
+                onPress={() => {
+                  Keyboard.dismiss()
+                  this.setState({ loading: true });
+                  this._verification();
+                }}
+                buttonStyle={styles.button}
+                textStyle={styles.buttonText} />
+              <ButtonIconRight text="RESEND"
+                icon="mail"
+                onPress={() => console.log('I should resend that email')}
+                buttonStyle={styles.button}
+                textStyle={styles.buttonText} />
+            </View>}
+          {this.state.loading && <ActivityIndicator color='#fff' />}
+          </KeyboardAvoidingView>
+          </ScrollView>
     );
+
+    // return(
+    //   <View
+    //         style={{ flex: 1 }}>
+    //         <Text height={0}>{'\n'}</Text>
+    //         {!this.state.loading &&
+    //           <Grid>
+    //             <Col style={{ height: 0 }}>
+    //               <Button
+    //                 title="RESEND"
+    //                 large
+    //                 raised
+    //                 fontSize={20}
+    //                 color='#333333'
+    //                 backgroundColor='rgb(247, 229, 59)'
+    //                 onPress={() => this.props.navigation.goBack(null)}
+    //                 style={{ width: 150, height: 80, alignItems: 'center' }} />
+    //             </Col>
+    //             <Col style={{ height: 0 }}>
+    //               <Button
+    //                 title="ENTER"
+    //                 large
+    //                 raised
+    //                 fontSize={20}
+    //                 color='#333333'
+    //                 backgroundColor='rgb(247, 229, 59)'
+    //                 onPress={() => {
+    //                   if (this.state.activationCode !== '' && this.state.activationCode.length == 6) {
+    //                     this.setState({ loading: true });
+    //                     this._verification();
+    //                   }
+    //                 }}
+    //                 style={{ width: 150, height: 80, alignItems: 'center' }} />
+    //             </Col>
+    //           </Grid>}
+    //         {this.state.loading &&
+    //           <ActivityIndicator color='#fff' />
+    //         }
+    //     </Image>
+    //         </View>
+    //     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
+          container: {
+          backgroundColor: colors.softBlue,
+    height: height,
+    width: width,
+  },
+  view: {
+          flex: 1,
+    alignItems: 'center'
+  },
+  button: {
+          marginTop: 20,
+    width: width - 100,
+    height: 40,
+  },
+  buttonText: {
+          fontSize: width / 20,
   },
   input: {
-    borderRadius: 10,
-    minWidth: 80,
-    width: 300,
-    flexWrap: 'wrap',
+          fontSize: width / 20,
+    width: width,
     height: 40,
-    backgroundColor: 'rgba(204,204,204,0.2)',
-    paddingHorizontal: 10,
-    color: '#333333',
     marginBottom: 10,
-  },
-  buttonContainer: {
-    backgroundColor: "#1980b9",
-    paddingVertical: 10,
-    marginTop: 15,
-    marginBottom: 20
-  },
-  loginbutton: {
-    color: '#ffffff',
+    color: colors.softGrey,
+    backgroundColor: colors.transparentTextEntry,
+    alignItems: 'center',
     textAlign: 'center',
-    fontWeight: '700'
-  }
+  },
 });
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     padding: 20,
+//   },
+//   input: {
+//     borderRadius: 10,
+//     minWidth: 80,
+//     width: 300,
+//     flexWrap: 'wrap',
+//     height: 40,
+//     backgroundColor: 'rgba(204,204,204,0.2)',
+//     paddingHorizontal: 10,
+//     color: '#333333',
+//     marginBottom: 10,
+//   },
+//   buttonContainer: {
+//     backgroundColor: "#1980b9",
+//     paddingVertical: 10,
+//     marginTop: 15,
+//     marginBottom: 20
+//   },
+//   loginbutton: {
+//     color: '#ffffff',
+//     textAlign: 'center',
+//     fontWeight: '700'
+//   }
+// });
